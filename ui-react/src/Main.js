33c429ab07d3fda817fcdb37f6cae01f9593cb9f
amalsanthoshp <amaldemaniax@gmail.com>
2019-10-23 20:58:09 +0530

    change style

--- ui-react/src/Main.js
+++ ui-react/src/Main.js
@@ -17,13 +17,26 @@ class Main extends Component {
 		return(
     <Router>
   		<div>
-        <div className="ui four item menu" id ="nav">
-          <a className="item"><NavLink exact to="/home">Home</NavLink></a>
-          <a className="item"><NavLink exact to="/character">Characters</NavLink></a>
-          <a className="item"><NavLink exact to="/search">Search</NavLink></a>
-          <a className="item"><NavLink exact to="/about">About</NavLink></a>
-        </div>
           <div className="container">
+          <div className="ui pointing menu" id ="nav">
+          <div className="header item">
+          <img className="ui avatar image" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fscreenrant.com%2Fwp-content%2Fuploads%2F2017%2F03%2FRickAndMortyFeature.jpg&f=1&nofb=1"/>
+            Rick and Morty
+
+          </div>
+          <a className="item">
+            <NavLink exact to="/home">Home</NavLink>
+          </a>
+          <a className="item">
+            <NavLink exact to="/character">Characters</NavLink>
+          </a>
+          <a className="item">
+            <NavLink exact to="/search">Search</NavLink>
+          </a>
+          <a className="item">
+            <NavLink exact to="/about">About</NavLink>
+          </a>
+        </div>
            <div>
               <img  className="ui image" src ='https://static3.srcdn.com/wordpress/wp-content/uploads/2018/04/Rick-and-Morty-Season-4-Logo.jpg' alt='image' style={{width:'100%',height:'100%'}}></img>
            </div>
--- ui-react/src/Pages/Home.js
+++ ui-react/src/Pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from '../Components/Card';
+import './page.css'
 
 class Home extends React.Component {
 
@@ -65,8 +66,35 @@ class Home extends React.Component {
           <div className='ui header'> Premise and Characters</div>
             <p>The show revolves around the adventures of the members of the Smith household, which consists of parents Jerry and Beth, their children Summer and Morty, and Beth's father, Rick Sanchez, who lives with them as a guest. According to Justin Roiland, the family lives outside of Seattle, Washington.[3] The adventures of Rick and Morty, however, take place across an infinite number of realities, with the characters travelling to other planets and dimensions through portals and Rick's flying car.</p>
             <p>Rick is an eccentric and alcoholic mad scientist, who eschews many ordinary conventions such as school, marriage, love, and family. He frequently goes on adventures with his 14-year-old grandson, Morty, a kind-hearted but easily distressed boy, whose naïve but grounded moral compass plays counterpoint to Rick's Machiavellian ego. Morty's 17-year-old sister, Summer, is a more conventional teenager who worries about improving her status among her peers and sometimes follows Rick and Morty on their adventures. The kids' mother, Beth, is a generally level-headed person and assertive force in the household, though self-conscious about her professional role as a horse surgeon. She is dissatisfied with her marriage to Jerry, a simple-minded and insecure person, who disapproves of Rick's influence over his family.</p>
-            <p>Different versions of the characters inhabit other dimensions throughout the multiverse and their personal characteristics can vary from one reality to another. The show's original Rick identifies himself as "Rick Sanchez of Earth Dimension C-137", in reference to his original universe, but this does not necessarily apply to every other member of the Smith household. For instance, in the first-season episode "Rick Potion #9", after turning the entire world population into monsters, Rick and Morty move to a different dimension, leaving Summer, Beth and Jerry behind. </p>
-       </div>     
+        <div className="ui segment">
+        <div className="ui two column very relaxed grid">
+          <div className="column">
+             <div className="ui link cards">
+              <div className="card" id='card'>
+                <div className="image">
+                <img src="https://d.ibtimes.co.uk/en/full/1645547/rick-morty.jpg"/>
+                </div>
+                <div className="content">
+                  <div className="header">Rick Sanchez and Morty Smith</div>
+                  <div className="meta">
+                    <a>Adult Swim</a>
+                  </div>
+                  <div className="description">
+                    A Scene from <a className="ui teal tag label">Season 4</a>
+                  </div>
+                </div>
+                </div>
+                </div>
+          </div>
+          <div className="column">
+              <p>Following the conclusion of the show's third season, co-creators Harmon and Roiland wanted to have assurance that there would be many more seasons of Rick and Morty in the future, so that they would be able to focus on the show and minimize their involvement in other projects.</p>
+              <p> Additionally, Harmon had expressed his wish for the upcoming fourth season to consist of more than ten episodes.</p>
+              <p> In May 2018, after prolonged contract negotiations, Adult Swim announced a long-term deal with the creators, ordering 70 new episodes over an unspecified number of seasons.</p>
+          </div>
+            <p> In a June 2018 interview with Polygon, Justin Roiland was confident that this big renewal deal will allow the creators to minimize the gaps between seasons, as they will be able to schedule their time around the show and "keep the machine going." In May 2019, at WarnerMedia's 2019 Upfronts presentation, the fourth season of Rick and Morty was announced to debut in November 2019. In a July 2019 interview with Entertainment Weekly, Roiland confirmed that the fourth season will consist of ten episodes.</p>
+        </div>
+      </div> 
+      </div>    
 		 </>
       )
   };
