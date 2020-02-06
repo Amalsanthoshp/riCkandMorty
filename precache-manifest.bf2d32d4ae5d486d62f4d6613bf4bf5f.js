157c55648973ac84610dfaf235ca673e65978bed
amal-san <amaldemaniax@gmail.com>
2020-02-06 11:14:47 +0530

    Updates

---
+++ precache-manifest.bf2d32d4ae5d486d62f4d6613bf4bf5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41807916b02833ad29d88930c42e3591",
+    "url": "/cronenberg-137/index.html"
+  },
+  {
+    "revision": "6e2b2c78cbf4ae77b9ec",
+    "url": "/cronenberg-137/static/css/main.e53bde79.chunk.css"
+  },
+  {
+    "revision": "1fab65c7671548acc2fd",
+    "url": "/cronenberg-137/static/js/2.d4b1b296.chunk.js"
+  },
+  {
+    "revision": "6e2b2c78cbf4ae77b9ec",
+    "url": "/cronenberg-137/static/js/main.fe60029a.chunk.js"
+  },
+  {
+    "revision": "69ca806c3eafa1fbde76",
+    "url": "/cronenberg-137/static/js/runtime-main.6929fa44.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cronenberg-137/home/precache-manifest.79500a2b70866341cdf9fd0ff3f23643.js"
+  "/cronenberg-137/precache-manifest.bf2d32d4ae5d486d62f4d6613bf4bf5f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cronenberg-137/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cronenberg-137/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
