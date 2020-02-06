4ea99ab8e7ddf931792bddaaf724e27c1c5514ee
amal-san <amaldemaniax@gmail.com>
2020-02-06 11:50:32 +0530

    add different route

--- ui-react/src/Main.js
+++ ui-react/src/Main.js
@@ -25,25 +25,25 @@ class Main extends Component {
 
           </div>
           <a className="item">
-            <NavLink exact to="/">Home</NavLink>
+            <NavLink exact to="/cronenberg-137/home">Home</NavLink>
           </a>
           <a className="item">
-            <NavLink exact to="/character">Characters</NavLink>
+            <NavLink exact to="/cronenberg-137/character">Characters</NavLink>
           </a>
           <a className="item">
-            <NavLink exact to="/search">Search</NavLink>
+            <NavLink exact to="/cronenberg-137/search">Search</NavLink>
           </a>
           <a className="item">
-            <NavLink exact to="/about">About</NavLink>
+            <NavLink exact to="/cronenberg-137/about">About</NavLink>
           </a>
         </div>
            <div>
               <img  className="ui image" src ='https://static3.srcdn.com/wordpress/wp-content/uploads/2018/04/Rick-and-Morty-Season-4-Logo.jpg' alt='image' style={{width:'100%',height:'100%'}}></img>
            </div>
-              <Route path="/" component={Home}/>
-              <Route path="/character" component={Character}/>
-              <Route path='/search' component={Search}/>
-              <Route path="/about" component={About}/> 
+              <Route path="/cronenberg-137/home" component={Home}/>
+              <Route path="/cronenberg-137/character" component={Character}/>
+              <Route path='/cronenberg-137/search' component={Search}/>
+              <Route path="/cronenberg-137/about" component={About}/> 
             </div>
       </div>
     </Router>
