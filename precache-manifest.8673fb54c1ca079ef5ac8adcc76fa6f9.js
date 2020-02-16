ce10c3cf1ae9bcdb56df5a3d474b2c48b178ea89
amal-san <amaldemaniax@gmail.com>
2020-02-16 18:15:51 +0530

    Updates

---
+++ precache-manifest.8673fb54c1ca079ef5ac8adcc76fa6f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b2977952ccf845c7a6590e59ac7bd29",
+    "url": "/cronenberg-137/index.html"
+  },
+  {
+    "revision": "63fb54049f9201564bb8",
+    "url": "/cronenberg-137/static/css/main.e53bde79.chunk.css"
+  },
+  {
+    "revision": "1fab65c7671548acc2fd",
+    "url": "/cronenberg-137/static/js/2.d4b1b296.chunk.js"
+  },
+  {
+    "revision": "63fb54049f9201564bb8",
+    "url": "/cronenberg-137/static/js/main.8fd90b9f.chunk.js"
+  },
+  {
+    "revision": "69ca806c3eafa1fbde76",
+    "url": "/cronenberg-137/static/js/runtime-main.6929fa44.js"
+  }
+]);
