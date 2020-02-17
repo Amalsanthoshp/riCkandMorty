60b13f501dc02567b5ab42468b5544b521c77f0d
amal-san <amaldemaniax@gmail.com>
2020-02-17 16:06:40 +0530

    img url

--- ui-react/src/Pages/About.js
+++ ui-react/src/Pages/About.js
@@ -8,7 +8,7 @@ class About extends React.Component {
 		return (
 		<div className='ui center aligned segment' id='about'>
 			<h2> About me </h2>
-			 <img className ="ui centered medium circular image" src="https://instagram.fcok1-1.fna.fbcdn.net/vp/26f4c4002d099936494350d9990c1246/5E2BFE83/t51.2885-15/e35/66950400_862466254139857_1548523287196076412_n.jpg?_nc_ht=instagram.fcok1-1.fna.fbcdn.net&_nc_cat=101"/><br></br>
+			 <img className ="ui centered medium circular image" src="https://avatars1.githubusercontent.com/u/42336393"/><br></br>
 
 			 <h3>Amal Santhosh</h3>
 			 <h5>Powered by React <i className="react icon" style={{color:'#0fbcbf'}}></i><i className="heart icon" style={{color:'red'}}></i>. </h5>
